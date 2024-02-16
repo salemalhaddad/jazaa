35e0ff7e5ccb1765131d02e78aabd5535cb6c288
Salem Al Haddad <salem.alhaddad@hotmail.com>
2024-02-16 16:02:03 +0400

    modify: problem statement

--- app/page.tsx
+++ app/page.tsx
@@ -91,7 +91,7 @@ export default function Home() {
       <section className="bg-gradient-to-r from-blue-500 to-blue-800 text-white">
         <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center px-6 md:px-12 py-20 md:py-32">
           <div className="md:w-1/2 space-y-6 md:space-y-8">
-            <h1 className="text-5xl font-bold leading-tight md:text-6xl md:leading-snug">Jazaa: Turn your one-time visitors into recurring customers</h1>
+            <h1 className="text-5xl font-bold leading-tight md:text-6xl md:leading-snug">Jazaa: Turn your one-time visitors into recurring customers using AI</h1>
             <p className="text-xl font-light leading-relaxed md:text-2xl md:leading-relaxed">
               Jazaa helps service businesses, like gyms and spas, increase their repeat customers through powerful automation.
             </p>
