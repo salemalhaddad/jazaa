5fe97f6cd10803f247ce922329adc68b0e12aba0
Salem Al Haddad <salem.alhaddad@hotmail.com>
2024-03-29 01:40:01 +0400

    removed fb config

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,16 +1,16 @@
-'use client'
-import React from "react";
-import { useAuthContext } from "../../context/AuthContext";
-import { useRouter } from "next/navigation";
-function Page() {
-    const { user } = useAuthContext()
-    const router = useRouter()
+// 'use client'
+// import React from "react";
+// import { useAuthContext } from "../../context/AuthContext";
+// import { useRouter } from "next/navigation";
+// function Page() {
+//     const { user } = useAuthContext()
+//     const router = useRouter()
 
-    React.useEffect(() => {
-        if (user == null) router.push("/")
-    }, [user, router])
+//     React.useEffect(() => {
+//         if (user == null) router.push("/")
+//     }, [user, router])
 
-    return (<h1>Only logged in users can view this page</h1>);
-}
+//     return (<h1>Only logged in users can view this page</h1>);
+// }
 
-export default Page;
+// export default Page;
