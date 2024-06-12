ce48404e5eb11670a0eb97ec0d7172dde724c4b2
Salem Al Haddad <salem.alhaddad@hotmail.com>
2024-06-12 16:14:07 +0400

    improved uploading page ui

--- src/app/onboarding/upload-data/page.tsx
+++ src/app/onboarding/upload-data/page.tsx
@@ -1,11 +1,7 @@
 'use client'
 import { useState } from 'react';
 import { cn } from "@/lib/utils";
-import { buttonVariants } from "@/components/ui/button";
-import { HomeIcon, ChartBarIcon, UsersIcon, FolderIcon, CashIcon } from '@heroicons/react/solid';
-import Link from "next/link";
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
-import Sidebar from "@/components/layout/Sidebar"
 import { useRouter } from "next/navigation";
 import { useSessionContext } from '@supabase/auth-helpers-react';
 import Papa from 'papaparse';
@@ -14,171 +10,129 @@ export default function Preferences() {
   const [customerDataFile, setCustomerDataFile] = useState<File | null>(null);
   const [uploadSuccess, setUploadSuccess] = useState(false);
   const supabaseClient = useSupabaseClient();
+  const { session } = useSessionContext();
+  const user = session?.user;
+  const router = useRouter();
 
-    const { session } = useSessionContext();
-    const user = session?.user;
+  const handleDataUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setCustomerDataFile(e.target.files[0]);
+    } else {
+      setCustomerDataFile(null);
+    }
+    setUploadSuccess(false); // Reset upload success state on new file selection
+  }
 
-    const router = useRouter();
-
-    const handleDataUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files) {
-            setCustomerDataFile(e.target.files[0]);
-        } else {
-            setCustomerDataFile(null);
-        }
-        setUploadSuccess(false); // Reset upload success state on new file selection
+  const handleCsvUpload = async () => {
+    const file = customerDataFile;
+    if (!file) {
+      return;
     }
 
-    
+    interface CsvRow {
+      customer_name: string;
+      whatsapp_no: string;
+      last_visit_date: string;
+      offering_name: string;
+      offering_price: number;
+      discount: string;
+      offering_amount: string;
+    }
 
-
-    const handleCsvUpload = async () => {
-        const file = customerDataFile;
-      
-        if (!file) {
+    Papa.parse<CsvRow>(file, {
+      header: true,
+      complete: async function (results) {
+        const { data, errors } = results;
+        if (errors.length > 0) {
+          console.error('Errors in parsing CSV:', errors);
           return;
         }
 
-        interface CsvRow {
-          customer_name: string;
-          whatsapp_no: string;
-          last_visit_date: string;
-          offering_name: string;
-          offering_price: number;
-          discount: string;
-          offering_amount: string;
-        }
-        
-      
-        Papa.parse<CsvRow>(file, {
-          header: true,
-          complete: async function (results) {
-            const { data, errors } = results;
-      
-            if (errors.length > 0) {
-              console.error('Errors in parsing CSV:', errors);
-              return;
+        for (const row of data) {
+          if (row) {
+            const uniqueIdentifier = row.customer_name;
+            if (!uniqueIdentifier) {
+              console.error('Unique identifier missing in row, skipping:', row);
+              continue;
             }
-      
-            for (const row of data) {
-              // Assuming there's a unique identifier in each row, like 'customer_id'
-              if (row) {
-                const uniqueIdentifier = row.customer_name; // Adjust based on actual unique identifier in your CSV/data structure
-                if (!uniqueIdentifier) {
-                  console.error('Unique identifier missing in row, skipping:', row);
-                  continue;
-                }
-                 // Add 'business' column to each row
-              // let businessName = "DefaultBusinessName"; // Fallback business name
-              // const { data: userData, error: userError } = await supabaseClient
-              //   .from('jazaa-users')
-              //   .select('business_name')
-              //   .eq('user_name', user?.user_metadata.full_name)
-              //   .single();
 
-              // if (!userError && userData) {
-              //   businessName = userData.business_name;
-              // }
-              
-              const rowWithBusiness = { ...row, business: user?.user_metadata.full_name };
+            const rowWithBusiness = { ...row, business: user?.user_metadata.full_name };
 
-              // Check if the row already exists in the database
-              const { data: existingRows, error: fetchError } = await supabaseClient
+            const { data: existingRows, error: fetchError } = await supabaseClient
+              .from('customer-visits')
+              .select('*')
+              .eq('customer_name', uniqueIdentifier);
+
+            if (fetchError) {
+              console.error('Error fetching existing rows:', fetchError);
+              continue;
+            }
+
+            if (existingRows.length === 0) {
+              const { data: { user } } = await supabaseClient.auth.getUser();
+              const { error: insertError } = await supabaseClient
                 .from('customer-visits')
-                .select('*')
-                .eq('customer_name', uniqueIdentifier); // Adjust the column name ('customer_id') as per your database schema
-
-                if (fetchError) {
-                  console.error('Error fetching existing rows:', fetchError);
-                  continue;
-                }
-  
-                if (existingRows.length === 0) {
-                  // Row does not exist, proceed to insert
-                  const { data: { user } } = await supabaseClient.auth.getUser()
-
-                                    
-                                
-                  // Encrypt the offering_price
-                  // const { data: encryptedOfferingPrice, error: encryptError } = await supabaseClient.rpc('pgp_sym_encrypt', {
-                  //   text: row.offering_price,
-                  //   key: 'iamtesting'
-                  // });
-
-                  // if (encryptError) {
-                  //   console.error('Error encrypting offering price:', encryptError);
-                  //   return;
-                  // }
-
-                  // Insert the row
-                  const { error: insertError } = await supabaseClient
-                    .from('customer-visits')
-                    .insert([
-                      {
-                        customer_name: row.customer_name,
-                        whatsapp_no: row.whatsapp_no,
-                        last_visit_date: row.last_visit_date,
-                        offering_name: row.offering_name,
-                        offering_price: row.offering_price,
-                        discount: row.discount,
-                        business: user?.user_metadata.full_name
-                      }
-                    ]);
-
-
-                  
-                    const { data, error: updateError } = await supabaseClient
-                    .rpc('encrypt_price', { last_date: row.last_visit_date })
-
-                  if (updateError) {
-                    console.error('Error updating offering_amount:', updateError)
-                  } else {
-                    console.log('Column offering_amount updated successfully.')
+                .insert([
+                  {
+                    customer_name: row.customer_name,
+                    whatsapp_no: row.whatsapp_no,
+                    last_visit_date: row.last_visit_date,
+                    offering_name: row.offering_name,
+                    offering_price: row.offering_price,
+                    discount: row.discount,
+                    business: user?.user_metadata.full_name
                   }
-                  
-                  if (insertError) {
-                    console.error('Error inserting row:', insertError);
-                  }
-                } else {
-                  console.log('Row already exists, skipping insert:', rowWithBusiness);
-                }
+                ]);
+
+              const { data, error: updateError } = await supabaseClient
+                .rpc('encrypt_price', { last_date: row.last_visit_date });
+
+              if (updateError) {
+                console.error('Error updating offering_amount:', updateError);
+              } else {
+                console.log('Column offering_amount updated successfully.');
               }
-        
-              console.log('CSV processing completed');
-              setUploadSuccess(true); // Set upload success state to true after processing is completed
 
-              }  
-          },
-        });
-      };
+              if (insertError) {
+                console.error('Error inserting row:', insertError);
+              }
+            } else {
+              console.log('Row already exists, skipping insert:', rowWithBusiness);
+            }
+          }
 
-    return (
-        <div className="flex h-screen bg-gray-100">
-        {/* <Sidebar /> */}
-        {/* Upload CSV Content */}
-        <div className="flex-1 p-10">
-            <h2 className={cn("text-2xl", "font-bold", "mb-4")}>Upload Customer Visit Data</h2>
-            <div className="flex flex-col">
-              <p className={cn("text-md", "mb-4", "text-red-600")}>Please ensure the CSV file contains columns for customer name, WhatsApp number, last visit time (MM/DD/YYYY), and the service/product they paid for.</p>
-              
-            </div>
-            <div className="flex flex-col ">
-                <label className={cn("text-lg", "font-medium", "mb-2")} htmlFor="uploadCsv">Upload CSV File</label>
-                <input
-                    type="file"
-                    id="uploadCsv"
-                    accept=".csv"
-                    onChange={handleDataUpload}
-                    className={cn("w-full", "px-4", "py-2", "text-lg", "border", "border-gray-300", "rounded")}
-                />
-                <a href="/customer_visits_sample.csv" download className={cn("text-md", "underline", "text-blue-600", "hover:text-blue-800")}>Download a sample CSV file</a>
-                <div className="mt-4">
-                    <button type="button" className="px-4 py-2 bg-primary text-white rounded" onClick={handleCsvUpload}>Upload</button>
-                </div>
-                {uploadSuccess && <p className={cn("text-md", "mt-4", "text-green-600")}>CSV upload successful!</p>}
-            </div>
+          console.log('CSV processing completed');
+          setUploadSuccess(true);
+        }
+      },
+    });
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <div className="w-full max-w-lg p-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">Upload Customer Visit Data</h2>
+        <p className="text-left text-gray-600 mb-4">Ensure the CSV file contains columns for customer name, WhatsApp number, last visit date, and the service/product they paid for. You can download a sample CSV file below.</p>
+        <div className="mb-4">
+          <label className="block text-lg font-medium text-gray-700 mb-2 mt-8" htmlFor="uploadCsv">Upload CSV File</label>
+          <input
+            type="file"
+            id="uploadCsv"
+            accept=".csv"
+            onChange={handleDataUpload}
+            className="w-full px-4 py-2 border border-gray-300 rounded-md"
+          />
+          <a href="/customer_visits_sample.csv" download className="block text-left text-blue-600 mt-2 underline">Download a sample CSV file</a>
         </div>
+        <button
+          type="button"
+          className="w-full px-4 py-2 bg-blue-600 text-white mt-2 rounded-md hover:bg-blue-700"
+          onClick={handleCsvUpload}
+        >
+          Upload
+        </button>
+        {uploadSuccess && <p className="text-center text-green-600 mt-4">CSV upload successful!</p>}
+      </div>
     </div>
-    );
+  );
 }
-
